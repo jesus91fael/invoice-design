bbb1cad1b1834e2eab00260f374c5a60cb1662e7
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-07-18 14:14:21 -0300

    chore: correção

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Title from './components/Title'
-import Product from "./components/Product";
-import Uniti from "./components/Uniti";
-import Container from "./components/Container";
-import Price from "./components/Price";
+import Table from "./components/Table";
 import Button from "./components/Button";
 import './App.css'
 
@@ -13,31 +10,7 @@ export default function App() {
   return (
     <div className='box'>
       <Title>Invoice</Title>
-      <Container className='row title'>
-        <Product className='product'>PRODUCT</Product>
-        <Uniti className='unidade'>UNIT</Uniti>
-        <Price className='preco'>Price</Price>
-      </Container>
-      <Container className='row conteudo'>
-        <Product className='product'>Thre Unicorns</Product>
-        <Uniti className='unidade'>1</Uniti>
-        <Price className='preco'>$15.00</Price>
-      </Container>
-      <Container className='row conteudo'>
-        <Product className='product'>Awesome Jacks</Product>
-        <Uniti className='unidade'>2</Uniti>
-        <Price className='preco'>$15.00</Price>
-      </Container>
-      <Container className='row conteudo'>
-        <Product className='product'>Two Kings</Product>
-        <Uniti className='unidade'>1</Uniti>
-        <Price className='preco'>$15.00</Price>
-      </Container>
-      <Container className='row soma'>
-        <Product className='product'>Total</Product>
-        <Uniti className='unidade'></Uniti>
-        <Price className='preco'>$60.00</Price>
-      </Container>
+      <Table/>
       <Button>Pay Now</Button>
     </div>
   );
---
+++ src/components/Table/index.jsx
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react'
+import './styles.css'
+
+const renderInvoices = (item, index) => {
+  return (
+    <div className='row' key={index}>
+      <div className='product'>{`${item.product}`}</div>
+      <div className='qtd'>{`${item.qtd}`}</div>
+      <div className='price'>{`${item.price}`}</div>
+      <hr />
+    </div>
+  )
+}
+
+
+const Table = (props) => {
+  const { fn1, fn2 } = props
+
+
+
+  return (
+    <>
+      <div className='row'>
+        <div className='product'>Product</div>
+        <div className='qtd'>Unit</div>
+        <div className='price'>Price</div>
+      </div>
+      <div>
+        {INVOICES.map(renderInvoices)}
+      </div>
+    </>
+  )
+}
+
+const INVOICES = [
+  {
+    'product': 'Three Unicorns',
+    'qtd': '1',
+    'price': '15'
+  },
+  {
+    'prouct': 'Awesome Jacks',
+    'qtd': '7',
+    'price': '15'
+  },
+  {
+    'product': 'Two Kings',
+    'qtd': '1',
+    'price': '30'
+
+  }
+]
+
+export default Table
